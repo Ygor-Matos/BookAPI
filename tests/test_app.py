62fe66f120c5c651cfbce21c8432d3fae7c3dee4
Ygor Matos <ygorsilvamatos@hotmail.com>
2025-04-13 15:07:46 -0300

    :white_check_mark: test: adicionando testes dos endpoints de  edicao e remocao de usuarios

--- tests/test_app.py
+++ tests/test_app.py
@@ -27,4 +27,65 @@ def test_create_user(client):
     }
 
 def test_read_users(client):
-    ...
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() =={ 'users': [
+        {
+            'id': 1,
+            'username': 'teste',
+            'email': 'teste@teste.com'            
+        }
+    ]
+    }
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+        'password': "mudei",
+        'username': 'dunossingo',
+        'email': 'teste@teste.com'
+        }
+    )
+
+    assert response.json() =={
+        'id': 1,
+        'username': 'dunossingo',
+        'email': 'teste@teste.com'
+    }
+
+
+def test_update_user_error(client):
+    response_max = client.put(
+        '/users/1000',
+        json={
+        'password': "mudei",
+        'username': 'dunossingo',
+        'email': 'teste@teste.com'
+        }
+    )
+
+    response_min = client.put(
+        '/users/0',
+        json={
+        'password': "mudei",
+        'username': 'dunossingo',
+        'email': 'teste@teste.com'
+        }
+    )
+
+    assert response_max.json() =={'detail':'user not found'} 
+    assert response_min.json() =={'detail':'user not found'} 
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == { 'message': 'user deleted'}
+
+def test_delete_user_error(client):
+    
+    response_max = client.delete('users/1000')
+    response_min = client.delete('users/0')
+
+    assert response_max.json() == {'detail':'user not found'}
+    assert response_min.json() == {'detail':'user not found'}
