68ab18977426e09f4ce3dd59cc27142976c307b3
Ygor Matos <ygorsilvamatos@hotmail.com>
2025-04-13 15:06:16 -0300

    :sparkles: feat: adicionando endpoints de edicao e delecao de usuarios

--- livraria/app.py
+++ livraria/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from http import HTTPStatus
 from livraria.schemas import Message
 from livraria.schemas import UserSchema, UserPublic, UserDB, UserList
@@ -23,3 +23,25 @@ def create_user(user:UserSchema):
 @app.get('/users/', response_model = UserList)
 def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id:int, user:UserSchema):
+
+    if user_id < 1 or user_id>len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail= 'user not found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id:int):
+    if user_id < 1 or user_id>len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail= 'user not found'
+        )
+    del database[user_id - 1]
+    return {'message':'user deleted'}
