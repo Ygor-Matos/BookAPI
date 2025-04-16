83614f90244f26390834a3629ca50ac9478a0a23
Ygor Matos <ygorsilvamatos@hotmail.com>
2025-04-16 17:46:31 -0300

    : white_check_mark: test: adicionando fixture de setup para testes no banco de dados

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,24 @@
 import pytest
 
 from fastapi.testclient import TestClient
+from livraria.models import table_registry
+
+from sqlalchemy.orm import Session
 
 from livraria.app import app
+from sqlalchemy import create_engine
 
 @pytest.fixture()
 def client():
     return TestClient(app)
+
+@pytest.fixture()
+def session():
+    #setup
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+    #teardown
+    table_registry.metadata.drop_all(engine)
