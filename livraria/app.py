fead9a3de190c57755975eb31f724ad72ab54d16
Ygor Matos <ygorsilvamatos@hotmail.com>
2025-10-09 15:51:02 -0300

    :sparkles: feat: atualizando endpoints para usar o banco de dados real

--- livraria/app.py
+++ livraria/app.py
@@ -1,57 +1,100 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from http import HTTPStatus
+from sqlalchemy import select
 from livraria.schemas import Message
-from livraria.schemas import UserSchema, UserPublic, UserDB, UserList
+from livraria.schemas import UserSchema, UserPublic, UserList
+
+from livraria.database import get_session
+from livraria.models import User
 
 app = FastAPI()
 
-database=[]
 
 @app.get('/', status_code=HTTPStatus.OK, response_model = Message)
 def read_root():
     return { 'message':'ola mundo'}
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model= UserPublic)
-def create_user(user:UserSchema):
-    user_with_id = UserDB(
-        id=len(database) +1, 
-        **user.model_dump()
+def create_user(user:UserSchema, session=Depends(get_session) ):
+
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) |  (User.email == user.email)
+            )
     )
-    database.append(user_with_id)
-    return user_with_id
+    if db_user:
+        if db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists'
+            )
+        elif db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists'
+            )
+
+    db_user=User(username=user.username, email=user.email, password=user.password)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 @app.get('/users/', response_model = UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+    limit: int = 10,
+    offset:int = 0,
+    session=Depends(get_session)
+   ):
+    user = session.scalars(
+        select(User).limit(limit).offset(offset)
+        )
+    return {"users":user}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id:int, user:UserSchema):
+def update_user(user_id:int, user:UserSchema, session = Depends(get_session)):
+    db_user= session.scalar(
+        select(User).where(User.id==user_id)
+    )
 
-    if user_id < 1 or user_id>len(database):
+    if not db_user:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail= 'user not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
+    db_user.email =user.email
+    db_user.username = user.username
+    db_user.password = user.password
 
-    return user_with_id
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    
+    return db_user
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id:int):
-    if user_id < 1 or user_id>len(database):
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail= 'user not found'
-        )
-    del database[user_id - 1]
-    return {'message':'user deleted'}
+def delete_user(user_id:int, session = Depends(get_session)):
+    db_user= session.scalar(
+        select(User).where(User.id==user_id)
+    )
 
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    session.delete(db_user)
+    session.commit()
+    
+    return {'message': 'User deleted'}
 
 @app.get('/users/{user_id}', response_model=UserPublic)
-def get_user(user_id:int):
-    if user_id<1 or user_id>len(database):
+def get_user(user_id:int, session = Depends(get_session)):
+    db_user= session.scalar(
+        select(User).where(User.id == user_id)
+    )
+    if not db_user:
         raise HTTPException(
-            status_code= HTTPStatus.NOT_FOUND, detail = 'user not found'
+            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
         )
-    user = database[user_id -1]
-    return user
+    return db_user
--- livraria/schemas.py
+++ livraria/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 class Message(BaseModel):
     message: str
 
@@ -11,9 +11,8 @@ class UserPublic(BaseModel):
     id: int 
     username: str
     email: EmailStr
+    model_config = ConfigDict(from_attributes=True)
 
-class UserDB(UserSchema):
-    id:int
 
 class UserList(BaseModel):
     users : list[UserPublic]
