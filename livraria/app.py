4f43996161cf559a6537d9972d2489a7b82d1b7a
Ygor Matos <ygorsilvamatos@hotmail.com>
2025-10-14 17:17:21 -0300

    :sparkles: feat: adicionando autenticação e autorização ao sistema

--- livraria/app.py
+++ livraria/app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+
 from http import HTTPStatus
 from sqlalchemy import select
 from livraria.schemas import Message
-from livraria.schemas import UserSchema, UserPublic, UserList
+from livraria.schemas import UserSchema, UserPublic, UserList, Token
 
 from livraria.database import get_session
 from livraria.models import User
+
+from livraria.security import get_password_hash, verify_password, create_access_token, get_current_user
 
 app = FastAPI()
 
@@ -34,7 +39,11 @@ def create_user(user:UserSchema, session=Depends(get_session) ):
                 detail='Username already exists'
             )
 
-    db_user=User(username=user.username, email=user.email, password=user.password)
+    db_user=User(username=user.username,
+                email=user.email, 
+                password=get_password_hash(user.password)
+            )
+    
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
@@ -45,7 +54,7 @@ def create_user(user:UserSchema, session=Depends(get_session) ):
 def read_users(
     limit: int = 10,
     offset:int = 0,
-    session=Depends(get_session)
+    session=Depends(get_session),
    ):
     user = session.scalars(
         select(User).limit(limit).offset(offset)
@@ -54,36 +63,32 @@ def read_users(
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id:int, user:UserSchema, session = Depends(get_session)):
-    db_user= session.scalar(
-        select(User).where(User.id==user_id)
-    )
-
-    if not db_user:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
-    db_user.email =user.email
-    db_user.username = user.username
-    db_user.password = user.password
-
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
+def update_user(user_id:int, user:UserSchema, session = Depends(get_session), current_user = Depends(get_current_user)):
     
-    return db_user
+    if current_user.id !=user_id:
+        raise HTTPException(status_code=400, detail='Not enough permission')
+
+    current_user.email =user.email
+    current_user.username = user.username
+    current_user.password = get_password_hash(user.password)
+
+    session.add(current_user)
+    session.commit()
+    session.refresh(current_user)
+    
+    return current_user
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id:int, session = Depends(get_session)):
-    db_user= session.scalar(
-        select(User).where(User.id==user_id)
-    )
-
-    if not db_user:
+def delete_user(user_id:int, session = Depends(get_session), current_user = Depends(get_current_user)):
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Not enough permissions'
         )
-    session.delete(db_user)
+
+
+
+    session.delete(current_user)
     session.commit()
     
     return {'message': 'User deleted'}
@@ -98,3 +103,22 @@ def get_user(user_id:int, session = Depends(get_session)):
             status_code=HTTPStatus.NOT_FOUND, detail="User not found"
         )
     return db_user
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session = Depends(get_session)
+):
+    user = session.scalar(
+        select(User).where(User.email == form_data.username)
+    )
+
+    if not user or not verify_password(form_data.password,user.password):
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+    
+    access_token = create_access_token(data={'sub':user.email})
+    
+    return {'access_token' : access_token, 'token_type':'Bearer'}
---
+++ livraria/security.py
@@ -0,0 +1,64 @@
+from pwdlib import PasswordHash
+
+from jwt import encode, decode
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from http import HTTPStatus
+
+from livraria.models import User
+from jwt.exceptions import PyJWTError # pode ser pyjwt
+
+from livraria.database import get_session
+pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = 'Minha senha secreta'
+ALGORITHM ='HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password,hashed_password)
+
+
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({'exp':expire})
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+
+def get_current_user(
+        session = Depends(get_session),
+        token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code = HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate':'Bearer'}
+    )
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username = payload.get('sub')
+        if not username:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+    user = session.scalar(
+        select(User).where(User.email == username)
+    )
+
+    if not user:
+        raise credentials_exception
+    
+    return user
